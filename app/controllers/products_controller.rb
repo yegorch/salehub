2c38717dd3e1bc7b123d0e7c7ebd0bf6258a63f9
yegorch <yegor.rb@gmail.com>
2021-11-12 16:57:37 +0600

    routes and link to product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,7 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
-  def create
+  def show
+    @product = Product.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
  root to: "products#index"
+
+ get '/products/:id', to: 'products#show', as: 'product'
+ get '/products', to: 'products#index'
 end
