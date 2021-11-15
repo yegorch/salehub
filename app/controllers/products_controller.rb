c48ca16cd5aa8f06521028f7efaf2f73502e825d
yegorch <yegor.rb@gmail.com>
2021-11-15 18:47:56 +0600

    products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,4 +10,16 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
   end
+
+  def create
+    @product = Product.new(product_params)
+    @product.save
+    redirect_to root_path
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :description, :image_url)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
  get '/products', to: 'products#index'
  get '/products/new', to: 'products#new', as: 'new_product'
  get '/products/:id', to: 'products#show', as: 'product'
+
+ post '/products', to: 'products#create'
 end
