82c01b25e036c03769c03cb01964ff8adcf65bb3
yegorch <yegor.rb@gmail.com>
2021-11-15 18:33:10 +0600

    new products and form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
  root to: "products#index"
 
- get '/products/:id', to: 'products#show', as: 'product'
  get '/products', to: 'products#index'
+ get '/products/new', to: 'products#new', as: 'new_product'
+ get '/products/:id', to: 'products#show', as: 'product'
 end
